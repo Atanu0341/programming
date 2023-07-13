e1d1063677071eeb8cc280f4e3fae94664fb2002
Atanu Majumder <atanumajumder2004@gmail.com>
2023-07-13 12:31:34 +0530

    scope

---
+++ c++/day4/scope.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=3;
+    cout<<a<<endl;
+
+    if(true){
+        int b =5;
+        cout<<b<<endl;
+    }
+
+    int b=10;
+    cout<<b<<endl;  
+
+    
+
+
+
+
+
+}
